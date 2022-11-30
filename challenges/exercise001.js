9bd8354c4ec7d5348e0714cea853e48cb1916084
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2022-11-30 19:07:42 +0000

    Completed Exercide 1 &2

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -11,46 +11,99 @@ export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
+  return firstName.substring(0,1).toUpperCase() + "." + lastName.substring(0,1).toUpperCase();
 }
 
 export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
+  //console.log(originalPrice+(originalPrice*vatRate/100));
+  return originalPrice+parseFloat((originalPrice*vatRate/100).toFixed(2));
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
+  return originalPrice-parseFloat((originalPrice*reduction/100).toFixed(2));
 }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
+  let position;
+  let  length;
+
+  if (str.length % 2 == 1) {
+    position = str.length / 2;
+    length = 1;
+  } else {
+    position = str.length / 2 - 1;
+    length = 2;
+  }
+
+  return str.substring(position, position + length)
 }
 
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  let newStr = "";
+
+  for (let i = 0; i < word.length; i++){
+    newStr+=word[word.length-i-1];
+  }
+return newStr;
 }
 
 export function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
+  for (let i=0; i < words.length; i++){
+    words[i] = reverseWord(words[i]);
+  }
+  return words;
 }
 
 export function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
+  let numberOfLinuxUsers = 0;
+  for (let i = 0; i < users.length; i++ ){
+    if (users[i].type=="Linux"){numberOfLinuxUsers++}
+  }
+  return numberOfLinuxUsers;
 }
 
 export function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
   // Add your code here!
+  let sum = 0;
+  for (let i = 0; i < scores.length; i++ ){
+    sum+=scores[i]
+  }
+  let mean =parseFloat((sum/scores.length).toFixed(2));
+  return mean;
 }
 
 export function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   // Add your code here!
+
+  if (n % 3 != 0 && n % 5 != 0) {
+    // not divisiable by both 3 and 5
+    return n;
+  }
+  else {
+    let result = "";
+    if (n % 3 == 0) {
+      result += "fizz";
+    }
+    if (n % 5 == 0) {
+      result += "buzz";
+    }
+    return result;
+  }
+
 }
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,40 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return (person.city =="Manchester");
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  const maxPeoplePerBus = 40;
+  return Math.ceil(people/maxPeoplePerBus);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  let numSheep = 0;
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] == "sheep"){numSheep++;};
+  }
+  return numSheep;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  
+  if ((person.address.postCode.substring(0,1)== "M") &&
+  (person.address.city == "Manchester")){
+      return true;
+    }
+    else {return false;}
+  
 }
